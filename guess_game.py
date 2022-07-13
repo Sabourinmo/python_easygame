2c10762705d34386e95e1e47ace26d0f904ebcee
Sabourinmo <50410020+Sabourinmo@users.noreply.github.com>
2022-07-13 16:47:48 -0400

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -5,7 +5,7 @@ def chooseRandNumber(difficulty = 10):
     askQuestion("please write down a number\n", secret_number)
     return secret_number
 
-def askQuestion(question = "Error please write down a number \n", secret_number = 0):
+def askQuestion(question = "please write down a number\n", secret_number = 0):
     try: 
         input_number = int(input(question))
         main(input_number, secret_number)
@@ -13,15 +13,13 @@ def askQuestion(question = "Error please write down a number \n", secret_number 
 
     except ValueError:
         print("Error please write down a number \n")
-        askQuestion()
+        askQuestion(secret_number)
 
 def main(input_number, secret_number):
     user_number = input_number
     print("secret key", secret_number)
 
-    if int(user_number) != int(secret_number):
-        askQuestion("your were wrong, please write down another number \n", secret_number)
-    else:
+    if int(user_number) == int(secret_number):
         print(f"Congrats, you guess the correct number {secret_number}\n")
         replay = input(("Do you want to play again? (y/n)\n"))
         
@@ -29,6 +27,8 @@ def main(input_number, secret_number):
             chooseRandNumber(20)
         else:
             pass
+    else:
+        askQuestion("your were wrong, please write down another number \n", secret_number)
 
 chooseRandNumber()
 
